a84f465d1d18a6b5587eef30085369a4e172ebf3
Lucas Silva <lucaslopessilva17@gmail.com>
2023-01-28 20:18:41 -0300

    volta oa padrão db

--- database/db.go
+++ database/db.go
@@ -2,7 +2,7 @@ package database
 
 import (
 	"log"
-//	"os"
+
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,7 +14,6 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-//	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASS")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("PORT")+" sslmode=disable"
 	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
@@ -22,4 +21,4 @@ func ConectaComBancoDeDados() {
 	}
 
 	DB.AutoMigrate(&models.Aluno{})
-	}
+}
